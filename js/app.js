aa82cea64b98288a3bfbd5f25abf2ccef21cdbf1
Belen-Bozzo <mbelenbozzo@gmail.com>
2021-08-31 08:44:24 -0600

    Added section elements to the button's event listener. Styled the new section.

--- js/app.js
+++ js/app.js
@@ -47,12 +47,33 @@ document.body.appendChild(button);
 
 //Add the event listener to the button
 button.addEventListener('click', function() {
-    //Create new section
-    var newSec = document.createElement(newSec);
-    newSec.innerText = 'New Section';
+    //Create new section, div, and paragraph with text
+    var newSec = document.createElement('section');
+    var newDiv = document.createElement('div');
+    var newTitle = document.createElement('h2');
+    newTitle.innerText = 'New Section';
 
+    var newPar = document.createElement('p');
+    newPar.innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.';
+   
+    newDiv.appendChild(newTitle);
+    newDiv.appendChild(newPar);
+    newSec.appendChild(newDiv);
+
+    // Style the new section
+    newDiv.classList.add('landing__container');
+   
+    //Add the new section to the page
+    var main = document.querySelector('main');
+    main.appendChild(newSec);
+
+    //Create new section link on navbar
+    var newSecLink = document.createElement('newSecLink');
+    newSecLink.innerText = 'New Section Link';
     //Add new section to the navbar
-    navbar.appendChild(newSec);
+    navbar.appendChild(newSecLink);
+
+
 })
 
 
