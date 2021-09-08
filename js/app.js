feeba497e2cef9bba67b700a4259fde7874597eb
Belen-Bozzo <mbelenbozzo@gmail.com>
2021-09-08 08:56:31 -0600

    Fixed section and link creation

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
-/**
- * 
+/** 
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
@@ -10,51 +9,31 @@
  * JS Version: ES2015/ES6
  * 
  * JS Standard: ESlint
- * 
 */
 
 //Select sections
-const sections = Array.from(document.querySelectorAll('section'));
+let sections = Array.from(document.querySelectorAll('section'));
 let numberOfSec = sections.length;
 
 //Select navigation ul
 const navbar = document.getElementById('navbar__list');
 
-//Create li (links on navbar) from the already existent sections
-function liCreator() {
-    for (section of sections) {
+function liCreator(){ 
     //Select the name and the link from data type and id
-    sectionName = section.getAttribute('data-nav');
-    sectionLink = section.getAttribute('id');
+    let sectionName = section.getAttribute('data-nav');
+    let sectionLink = section.getAttribute('id');
 
-    listItem = document.createElement('li');
+    let listItem = document.createElement('li');
     listItem.innerHTML = `<a class='menu__link' data-link='${sectionLink}'>${sectionName}</a>`;
-
     navbar.appendChild(listItem);
+}
+
+function buildNav(){
+    for (section of sections) {
+        liCreator();
     }
 }
-liCreator();
-
-// Scroll to section on link click
-
-
-//Old approach - event listener for each item
-//const links = document.querySelectorAll('.menu__link');
-// links.forEach((item) => {
-//    item.addEventListener("click", () => {
-//        console.log(item)
-//    })
-//})
-
-//Event Delegation
-
-//const parent = document.getElementById("navbar__list");
-
-//parent.addEventListener('click', event => {
-//    if (event.target.className === 'menu__link') {
-//        console.log(event.target.innerHTML);
-//   }
-//});
+buildNav();
 
 
 // Create button
@@ -64,71 +43,43 @@ button.innerText = 'Create Section';
 //Add the button to body
 document.body.appendChild(button);
 
-
+//Function that creates sections (for the button)
 function sectionCreator() {
+    let sections = Array.from(document.querySelectorAll('section'));
+    let numberOfSec = sections.length;
+
     //Create new section by cloning section 1
     const copySec = document.querySelector('#section1');
-    const newSec = copySec.cloneNode(true);
-    
+    let section = copySec.cloneNode(true);
 
+    //Add new section to array
+    sections.push(section);
+    
     //Give an id and data-nav to the new section
-    newSec.setAttribute('id','section'+ (numberOfSec + 1));
-    newSec.setAttribute('class', '');
-    newSec.setAttribute('data-nav', 'Section'+' '+(numberOfSec+1));
+    section.setAttribute('id','section'+ (numberOfSec + 1));
+    section.setAttribute('class', '');
+    section.setAttribute('data-nav', 'Section'+' '+(numberOfSec+1));
     numberOfSec += 1;
 
     //Give appropiate title to the new section
-    const clonedDiv = newSec.children[0]; 
+    const clonedDiv = section.children[0]; 
     const clonedTitle = clonedDiv.children[0];
-    const newTitle = newSec.getAttribute('data-nav');
+    const newTitle = section.getAttribute('data-nav');
     clonedTitle.innerHTML = newTitle;
 
     //Add the new section to the page
     const main = document.querySelector('main');
-    main.appendChild(newSec);
+    main.appendChild(section);
 
-    //Create new section link on navbar
-    sectionName = newSec.getAttribute('data-nav');
-    sectionLink = newSec.getAttribute('id');
-    listItem = document.createElement('li');
-    listItem.innerHTML = `<a class='menu__link' data-link='${sectionLink}'>${sectionName}</a>`;
-    navbar.appendChild(listItem);
-
-
-
-
-     //OLD CODE FOR CREATING THE NEW SECTION
-
-     // const newSec = document.createElement('section');
-      //const newDiv = document.createElement('div');
-      //const newTitle = document.createElement('h2');
-  
-      //Give an id and data-nav to the new section
-     // newSec.setAttribute('id', 'section'+(numberOfSec+1));
-      
-      //Add title with increasing number
-     // newTitle.innerText = ' Section '+ (numberOfSec + 1) + ' ';
-     // numberOfSec += 1 ;
-  
-      //const newPar = document.createElement('p');
-     // newPar.innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.';
-     
-      //Add new div and paragraph
-     // newDiv.appendChild(newTitle);
-     // newDiv.appendChild(newPar);
-     // newSec.appendChild(newDiv);
-  
-      // Style the new div
-      //newDiv.classList.add('landing__container');
-     
-       //Create new section link on navbar
-      //newSecName = newSec.getAttribute('data-nav');
-      //newSecLink = newSec.getAttribute('id');
-      //listItem = document.createElement('li');
-     // listItem.innerHTML = `<a class='menu__link' data-link='${newSecLink}'>${newSecName}</a>`;
-     // navbar.appendChild(listItem);
-  
-      
+    //Add the links on navbar
+    function addNewLinks(){
+        let sectionName = section.getAttribute('data-nav');
+        let sectionLink = section.getAttribute('id');
+        var listItem = document.createElement('li');
+        listItem.innerHTML = `<a class='menu__link' data-link='${sectionLink}'>${sectionName}</a>`;
+        navbar.appendChild(listItem);
+    }
+    addNewLinks();
 
 }
 
@@ -136,9 +87,8 @@ function sectionCreator() {
 button.addEventListener('click', sectionCreator);
 
 
-//Add functionality to navbar links
+//Add functionality to navbar links (event delegation)
 const parent = document.getElementById("navbar__list");
-
 parent.addEventListener('click', event => {
     if (event.target.className === 'menu__link') {
         let el = document.getElementById((sectionLink)); // Resolver :Como linkear los id de las secciones y los link?
@@ -146,11 +96,6 @@ parent.addEventListener('click', event => {
    }
 });
 
-
-
-
-
- 
 
 // Add class 'active' to section when near top of viewport
 
@@ -172,5 +117,3 @@ parent.addEventListener('click', event => {
 
 
 // Set sections as active
-
-
