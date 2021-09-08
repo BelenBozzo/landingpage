facd8b7d3bbdc0228c7164ccd72d9d82c78838d7
Belen-Bozzo <mbelenbozzo@gmail.com>
2021-09-08 12:27:29 -0600

    Manually added scroll to top button, styled and given functionality.

--- js/app.js
+++ js/app.js
@@ -39,15 +39,14 @@ buildNav();
 // Create button
 const button = document.createElement('button');
 button.innerText = 'Create Section';
+button.setAttribute('id', 'newSecButton');
 
 //Add the button to body
 document.body.appendChild(button);
 
 //Function that creates sections (for the button)
 function sectionCreator() {
-    let sections = Array.from(document.querySelectorAll('section'));
-    let numberOfSec = sections.length;
-
+   
     //Create new section by cloning section 1
     const copySec = document.querySelector('#section1');
     let section = copySec.cloneNode(true);
@@ -87,14 +86,43 @@ function sectionCreator() {
 button.addEventListener('click', sectionCreator);
 
 
-//Add functionality to navbar links (event delegation)
+//Add functionality to navbar links (event delegation: works with future  created elements)
 const parent = document.getElementById("navbar__list");
 parent.addEventListener('click', event => {
     if (event.target.className === 'menu__link') {
-        let el = document.getElementById((sectionLink)); // Resolver :Como linkear los id de las secciones y los link?
-        el.scrollIntoView({behavior: 'smooth'}); //Scroll funciona
+        let navBarLink = event.target.getAttribute('data-link');
+        for (section of sections){
+            let sectionLink = section.getAttribute('id');
+            if (sectionLink === navBarLink){
+                let el = document.getElementById((sectionLink));
+                el.scrollIntoView({behavior: 'smooth'});
+            }
+        }
    }
 });
+
+//Add functionality to scroll to top button
+//Get the button:
+topButton = document.getElementById("topButton");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topButton.style.display = "block";
+  } else {
+    topButton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+  }) 
+}
 
 
 // Add class 'active' to section when near top of viewport
